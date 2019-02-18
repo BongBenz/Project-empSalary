4cca167763fd3473664baa76c0a1df7d1a997b6c
juttimad <kartai37505@gmail.com>
2019-02-18 11:18:31 +0700

    Add directory empSalaryC

---
+++ empSalaryC/src/empSalaryC/outputProjectC.java
@@ -0,0 +1,10 @@
+package empSalaryC;
+
+public class outputProjectC {
+	public outputProjectC(){
+		
+	}
+	public outputProjectC(inputProjectC in,processProjectC cess){
+		System.out.println("Name : "+in.getName()+"\nTotal : "+cess.getSum());
+	}
+}
